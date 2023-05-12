d778ecd6aad670965bb240e5405831d0697cac37
mohcine <mohcine.badour@uit.ac.ma>
2023-05-12 17:14:11 +0100

    cavani

--- resources/views/livewire/multi-step-form.blade.php
+++ resources/views/livewire/multi-step-form.blade.php
@@ -302,25 +302,25 @@
                                 <span class="progress-count">1</span>
                                 <span class="progress-label">Billing Info</span>
                             </li>
-                            <li class="step-wizard-item ">
-                                <span class="progress-count ">2</span>
-                                <span class="progress-label">Payment Method</span>
-                            </li>
-                            <li class="step-wizard-item ">
-                                <span class="progress-count">3</span>
-                                <span class="progress-label">Checkout</span>
-                            </li>
-                            <li class="step-wizard-item ">
-                                <span class="progress-count">4</span>
-                                <span class="progress-label">Success</span>
-                            </li>
-                        </ul>
-                    </section>
-                    {{-- page 1 --}}
-                    @if ($currentStep == 1)
-                        <div class="step-one form first">
-                            <div class="details personal">
-                                <span class="title">My Family</span>
+                        <li class="step-wizard-item current-item">
+                            <span class="progress-count ">2</span>
+                            <span class="progress-label">Payment Method</span>
+                        </li>
+                        <li class="step-wizard-item ">
+                            <span class="progress-count">3</span>
+                            <span class="progress-label">Checkout</span>
+                        </li>
+                        <li class="step-wizard-item ">
+                            <span class="progress-count">4</span>
+                            <span class="progress-label">Success</span>
+                        </li>
+                    </ul>
+                </section>
+                {{-- page 1 --}}
+                @if ($currentStep == 1)
+                    <div class="step-one form first">
+                        <div class="details personal">
+                            <span class="title">My Family</span>
 
                                 <div class="fields" style="width: 99%;">
                                     {{-- <div class="input-field">
